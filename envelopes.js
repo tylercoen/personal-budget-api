838b7cc7288dd5281f50070ba64a0a21e0e5b753
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2025-05-02 17:46:40 -0400

    Creat POS endpoint, update GET

--- envelopes.js
+++ envelopes.js
@@ -15,4 +15,33 @@ router.get("/", (req, res) => {
   res.json(envelopes);
 });
 
+// POST: create a new envelope
+router.post("/", (req, res) => {
+  const { title, budget } = req.body;
+
+  //basic validation
+  if (!title || typeof budget !== "number" || budget < 0) {
+    return res.status(400).json({ error: "Invalid envelope data" });
+  }
+
+  //generate new ID
+  const newId =
+    envelopes.length > 0 ? envelopes[envelopes.length - 1].id + 1 : 1;
+  const newEnvelope = { id: newId, title, budget };
+
+  envelopes.push(newEnvelope);
+  res.status(201).json(newEnvelope);
+});
+
+// GET envelop by id
+router.get("/:id", (req, res) => {
+  const envelopeId = parseInt(req.params.id);
+  const envelope = envelopes.find((env) => env.id === envelopeId);
+
+  if (!envelope) {
+    res.status(404).json({ error: "Envelope not found" });
+  }
+  res.json(envelope);
+});
+
 module.exports = router;
