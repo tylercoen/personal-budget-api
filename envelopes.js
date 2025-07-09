aea5c95369412bb278f929d1fce5f4bb57b44744
Tyler Coen <43471099+tylercoen@users.noreply.github.com>
2025-07-09 21:36:17 +0000

    complete and test DELETE

--- envelopes.js
+++ envelopes.js
@@ -45,7 +45,7 @@ router.post("/", async (req, res) => {
 
 // GET envelope by id
 router.get("/:id", async (req, res) => {
-  const envelopeId = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
   try {
     const result = await pool.query("SELECT * FROM envelopes WHERE id = $1", [
       id,
@@ -62,14 +62,14 @@ router.get("/:id", async (req, res) => {
 
 // PUT: udpate an envelope by ID
 router.put("/:id", async (req, res) => {
-  const envelopeId = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
   const { withdraw, amount, name } = req.body;
   let messageParts = [];
 
   try {
     //retrieve the envelope
     const { rows } = await pool.query("SELECT * FROM envelopes WHERE id = $1", [
-      envelopeId,
+      id,
     ]);
     const envelope = rows[0];
     if (!envelope) {
@@ -104,7 +104,7 @@ router.put("/:id", async (req, res) => {
 
     const updated = await pool.query(
       "UPDATE envelopes SET name =  $1, amount = $2 WHERE id = $3 RETURNING *",
-      [newName, newAmount, envelopeId]
+      [newName, newAmount, id]
     );
 
     //send conf message
@@ -118,23 +118,29 @@ router.put("/:id", async (req, res) => {
   }
 });
 
-//This all still isn't updated yet.
-
 // DELETE
-router.delete("/:id", (req, res) => {
-  const envelopeId = parseInt(req.params.id);
-  const index = envelopes.findIndex((env) => env.id === envelopeId);
-  if (index === -1) {
-    res.status(400).json({ error: "Envelope not found" });
+router.delete("/:id", async (req, res) => {
+  const id = parseInt(req.params.id);
+  try {
+    const result = await pool.query(
+      "DELETE FROM envelopes WHERE id = $1 RETURNING *",
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Envelope not found" });
+    }
+
+    res.json({
+      message: `Successfully deleted enevelop ${result.rows[0].name}`,
+      deletedEnvelope: result.rows[0],
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ error: "Database error" });
   }
-  //remove the envelope
-  const deleteEnvelope = envelopes.splice(index, 1)[0];
-  res.status(200).json({
-    message: `Successfully deleted envelope ${deleteEnvelope.name}`,
-    deleteEnvelope,
-  });
 });
 
+// POST
 router.post("/transfer/:fromId/:toId", (req, res) => {
   const fromId = parseInt(req.params.fromId);
   const toId = parseInt(req.params.toId);
@@ -143,30 +149,6 @@ router.post("/transfer/:fromId/:toId", (req, res) => {
   if (isNaN(fromId) || isNaN(toId)) {
     return res.status(400).json({ error: "Invalid envelope IDs" });
   }
-  if (typeof amount !== "number" || amount <= 0) {
-    return res
-      .status(400)
-      .json({ error: "Transfer amount must be a positive number" });
-  }
-
-  const fromEnvelope = envelopes.find((env) => env.id === fromId);
-  const toEnvelope = envelopes.find((env) => env.id === toId);
-  if (!fromEnvelope || !toEnvelope) {
-    res.status(404).json({ error: "One or both envelopes not found" });
-  }
-  if (fromEnvelope.amount < amount) {
-    return res
-      .status(400)
-      .json({ error: "Insufficient funds in the source envelope" });
-  }
-  fromEnvelope.amount -= amount;
-  toEnvelope.amount += amount;
-
-  res.status(200).json({
-    message: `Transferred $${amount} from ${fromEnvelope.name} to ${toEnvelope.name}.`,
-    from: fromEnvelope,
-    to: toEnvelope,
-  });
 });
 
 module.exports = router;
